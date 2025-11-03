951eba76306274d0e0f4de2711ba2acace157bbd
Brandon Hedrick <brah.edrick@proton.me>
2025-11-02 19:28:33 -1000

    Fix a small discoloration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <Providers>
-          <Container maxW="4xl">
+          <Container maxW="4xl" minH="100vh" bg="bg">
             <Navigation />
             {children}
           </Container>
